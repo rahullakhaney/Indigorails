6d55d9ee26a46b87acff16c69eaaa07794f16436
RAHUL LAKHANEY <rahul.lakhaney@gmail.com>
2016-01-31 16:28:32 +0530

    Fixed a silly mistake

--- app/models/step.rb
+++ app/models/step.rb
@@ -1,3 +1,7 @@
 class Step < ActiveRecord::Base
   belongs_to :course
+  validates :title, presence: true, length: { maximum: 50 }
+validates :video, presence: true
+validates :tag, presence: true
+validates :course, presence: true
 end
